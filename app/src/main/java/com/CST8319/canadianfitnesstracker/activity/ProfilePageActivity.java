d48ef1897a7e623197f4b8334888ec8236d0128f
vdunstan1-rgb <vdunstan1@gmail.com>
2026-03-21 09:52:42 -0400

    Profile page template added

---
+++ app/src/main/java/com/CST8319/canadianfitnesstracker/activity/ProfilePageActivity.java
@@ -0,0 +1,19 @@
+package com.CST8319.canadianfitnesstracker.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.CST8319.canadianfitnesstracker.R;
+
+public class ProfilePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+
+
+    }
+
+}
